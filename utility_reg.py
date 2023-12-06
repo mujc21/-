03fbfd5bde4a50983fde066e993dd0cb8bdea02b
mujc21 <90122406+mujc21@users.noreply.github.com>
2023-12-06 21:40:20 +0800

    Add files via upload

--- utility_reg.py
+++ utility_reg.py
@@ -1,18 +1,39 @@
 import glob
 import os
 
+import imageio
 import open3d as o3d
 import numpy as np
 import time
 import math,random
 from math import sin, cos
-import imageio
-import cv2
+import torch
+import matplotlib as plt
+from cv2 import cv2
 
 voxel_size = 1.0
 ransac_n = 3
 distance_threshold = 1.8
 num_iter = 10000
+
+def n2o(__points) -> o3d.geometry.PointCloud:
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(__points[:, :3])
+    if __points.shape[1] == 6:
+        pcd.colors = o3d.utility.Vector3dVector(__points[:, 3:])
+    return pcd
+
+def o2n(__pcd):
+    return np.concatenate((np.asarray(__pcd.points), np.asarray(__pcd.colors)), axis=-1)
+
+def apply_transform_np(points, transform=None, rotation=None, translation=None):
+    if transform is not None:
+        rotation = transform[:3, :3]
+        translation = transform[:3, 3]
+    cord = points[:, :3]
+    cord = np.transpose(rotation @ np.transpose(cord) + translation.reshape(3, 1))
+    return np.concatenate((cord, points[:, 3:]), -1)
+
 
 def get_rot_matrix(theta, axis):
     R = [
@@ -68,6 +89,14 @@ def ext_fpfh(pcd, dp, radius_normal, radius_feature):
 
     return pcd_fpfh, pcd_c_fpfh
 
+def get_color(color_name):
+    if color_name == "custom_yellow":
+        return np.asarray([255.0, 204.0, 102.0]) / 255.0
+    if color_name == "custom_blue":
+        return np.asarray([102.0, 153.0, 255.0]) / 255.0
+    assert color_name in plt.colors.CSS4_COLORS
+    return np.asarray(plt.colors.to_rgb(plt.colors.CSS4_COLORS[color_name]))
+
 def preprocess_point_cloud_with_ext_fpfh(pcd, dp, voxel_size):
     # print(":: Downsample with a voxel size %.3f." % voxel_size)
     # pcd_down = pcd.voxel_down_sample(voxel_size)
@@ -103,20 +132,51 @@ def preprocess_point_cloud(pcd, voxel_size):
 
     return pcd_down, pcd_fpfh
 # 怎么感觉颜色信息没有用上呀？？？
-def draw_registration_result_np(pcd0_np, pcd1_np):
+def draw_registration_result_point_with_color(pcd0_np, pcd1_np, use_color0=False, use_color1=False, transform = None):
+    # pcd0是src点云
+    if transform is not None:
+        src_points = pcd0_np[:, :3]
+        src_points = apply_transform_np(src_points, transform)
+        pcd0_np = np.concatenate([src_points, pcd0_np[:, 3:]], 1)
     pcd0 = o3d.geometry.PointCloud()
     pcd1 = o3d.geometry.PointCloud()
-    pcd0.points = o3d.utility.Vector3dVector(pcd0_np)
-    pcd1.points = o3d.utility.Vector3dVector(pcd1_np)
-    points0 = pcd0_np
-    points1 = pcd1_np
-    color1 = np.array([1, 0, 0]).reshape(1, -1)
-    color1 = np.repeat(color1, points0.shape[0], axis=0)
-    color2 = np.array([0, 0, 1]).reshape(1, -1)
-    color2 = np.repeat(color2, points1.shape[0], axis=0)
-    pcd0.colors = o3d.utility.Vector3dVector(color1)
-    pcd1.colors = o3d.utility.Vector3dVector(color2)
+    if use_color0:
+        pcd0.points = o3d.utility.Vector3dVector(pcd0_np[:, :3])
+    else:
+        pcd0.points = o3d.utility.Vector3dVector(pcd0_np[:, :3])
+    if use_color1:
+        pcd1.points = o3d.utility.Vector3dVector(pcd1_np[:, :3])
+    else:
+        pcd1.points = o3d.utility.Vector3dVector(pcd1_np[:, :3])
+    pcd0.estimate_normals()
+    pcd1.estimate_normals()
+    if use_color0:
+        color0 = pcd0_np[:, 3:]
+        pcd0.colors = o3d.utility.Vector3dVector(color0)
+    else:
+        pcd0.paint_uniform_color(get_color('custom_blue'))
+    if use_color1:
+        color1 = pcd1_np[:, 3:]
+        pcd1.colors = o3d.utility.Vector3dVector(color1)
+    else:
+        pcd1.paint_uniform_color(get_color('custom_yellow'))
     o3d.visualization.draw_geometries([pcd0, pcd1])
+
+
+
+def draw_single_pcd(pcd_np, use_color=False):
+    pcd = o3d.geometry.PointCloud()
+    if use_color:
+        pcd.points = o3d.utility.Vector3dVector(pcd_np[:, :3])
+    else:
+        pcd.points = o3d.utility.Vector3dVector(pcd_np[:, :3])
+    pcd.estimate_normals()
+    if use_color:
+        color = pcd_np[:, 3:]
+        pcd.colors = o3d.utility.Vector3dVector(color)
+    else:
+        pcd.paint_uniform_color(get_color("custom_blue"))
+    o3d.visualization.draw_geometries([pcd])
 
 def draw_registration_result(pcd0, pcd1):
     # return
@@ -193,17 +253,7 @@ def execute_global_registration(source_down, target_down, source_fpfh,
         ], o3d.pipelines.registration.RANSACConvergenceCriteria(num_iter, 0.999))
     return result
 
-def apply_transformation(source, transformation, debug=False):
-    if debug:
-        print(transformation)
-        print("")
-    src_np = np.asarray(source.points)
-    T = transformation
-    t_src_np = np.ones((src_np.shape[0], 4))
-    t_src_np[:, :3] = src_np
-    t_src_np = t_src_np @ np.transpose(T) # 矩阵乘法
-    source.points = o3d.utility.Vector3dVector(t_src_np[:, :3])
-    return source
+
 # icp算法进行局部配准
 def run_icp(src, tar):
     # return src, tar
@@ -217,61 +267,6 @@ def run_icp(src, tar):
     # src = apply_transformation(src, reg_p2p.transformation)
     print(reg_p2p)
     return src, tar, reg_p2p
-
-# def registration(src_np, tar_np):
-#     source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset_np_pt(src_np, tar_np,
-#                                                                                          voxel_size, deg, axis, fout)
-#     target_raw = o3d.geometry.PointCloud()
-#     target_raw.points = o3d.utility.Vector3dVector(tar_np)
-#     start = time.time()
-#     result_ransac = execute_global_registration(source_down, target_down, source_fpfh, target_fpfh,
-#                                                 ransac_n, distance_threshold, num_iter)
-#
-#     source = apply_transformation(source, result_ransac.transformation)
-#     # source.transform(result_ransac.transformation)
-#     #
-#     # src_np = np.asarray(source.points)
-#     # T = result_ransac.transformation
-#     # t_src_np = np.ones((src_np.shape[0], 4))
-#     # t_src_np[:, :3] = src_np
-#     # t_src_np = t_src_np @ np.transpose(T)
-#     # source.points = o3d.utility.Vector3dVector(t_src_np[:, :3])
-#
-#     #
-#     if fout is not None:
-#         fout.write('''{}\n\n'''.format(result_ransac.transformation))
-#     else:
-#         print(result_ransac.transformation)
-#     end = time.time()
-#     source, target = run_icp(source, target, fout)
-#     print(f"inference time: {end - start} s")
-#     draw_registration_result(source, target_raw)
-#
-# def reg_partial_partial(src_np, tar_np, deg=(0,0,0), axis='N', fout=None):
-#     source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset_np_pp(src_np, tar_np,
-#                                                                                          voxel_size, deg, fout)
-#     start = time.time()
-#     result_ransac = execute_global_registration(source_down, target_down, source_fpfh, target_fpfh,
-#                                                 ransac_n, distance_threshold, num_iter)
-#     source.transform(result_ransac.transformation)
-#     if fout is not None:
-#         fout.write('''{}\n\n'''.format(result_ransac.transformation))
-#     else:
-#         print(result_ransac.transformation)
-#     source, target = run_icp(source, target, fout)
-#     end = time.time()
-#     print(f"inference time: {end - start} s")
-#     draw_registration_result(source, target)
-
-def n2o(__points, __colors=None) -> o3d.geometry.PointCloud:
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(__points)
-    if __colors is not None:
-        pcd.colors = o3d.utility.Vector3dVector(__colors)
-    return pcd
-
-def o2n(__pcd):
-    return np.asarray(__pcd.points), np.asarray(__pcd.colors)
 
 def _back_projection(img, depth, intrinsic, pose=None):
     # pass
@@ -298,6 +293,7 @@ def _back_projection(img, depth, intrinsic, pose=None):
         homo_pts = homo_pts @ np.transpose(pose)
     homo_pts = homo_pts / homo_pts[:, 3:]
     return homo_pts[:, :3], color  # img, depth_min, depth_max
+
 def gen_integrated_pcd_from_rgbd_images(base_folder):
     intrisic_path = os.path.join(base_folder, 'camera-intrinsics.txt')
     intrinsic = np.loadtxt(intrisic_path)
@@ -331,9 +327,20 @@ def gen_integrated_pcd_from_rgbd_images(base_folder):
             pcd = pcd.voxel_down_sample(0.01)
         pcds[os.path.split(seq)[-1]] = pcd
     return pcds
-    # add 下采样
-    # pcd = pcd.voxel_down_sample(voxel_size=0.01)
-    # mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha=0.1)
 
-    # o3d.visualization.draw_geometries([pcd])
+def get_rotation_matrix_from_axis_and_degree(axis_of_rotation, angle_degrees):  # 旋转角度（以度为单位）
+    angle_radians = np.radians(angle_degrees)  # 将角度转换为弧度
+    # 使用 Rodrigues' 公式计算旋转矩阵
+    rotation_matrix = np.eye(3) + np.sin(angle_radians) * np.cross(np.eye(3), axis_of_rotation) + (
+                1 - np.cos(angle_radians)) * np.outer(axis_of_rotation, axis_of_rotation)
+    return rotation_matrix
+
+def ply2npy(pcd):
+    cords = np.array(pcd.points)
+    colors = np.array(pcd.colors)
+    pcd = np.concatenate((cords, colors), -1)
+    return pcd
+
+
+
 
